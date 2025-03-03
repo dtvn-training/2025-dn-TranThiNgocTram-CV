aa36d85c08d9c87051fa07e9abf544cd20e82c0d
jasmineChamCham <jasminebkdn@gmail.com>
2025-03-03 15:53:06 +0700

    fix: remove redundant code, add smooth scrolling effect, adjust target of a tags, check responsive

--- js/utils.js
+++ js/utils.js
@@ -2,13 +2,11 @@ document.addEventListener("DOMContentLoaded", function () {
     const burgerMenu = document.getElementById("burger-menu");
     const navbar = document.getElementById("navbar");
 
-    // Toggle the menu when clicking the burger icon
     burgerMenu.addEventListener("click", function () {
         navbar.classList.toggle("active");
         burgerMenu.classList.toggle("active");
     });
 
-    // Close the menu when clicking a link
     document.querySelectorAll(".navbar a").forEach(link => {
         link.addEventListener("click", function () {
             navbar.classList.remove("active");
