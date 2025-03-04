82ac5ef57a26fa7c8a9aae1a0a231727813bf459
jasmineChamCham <jasminebkdn@gmail.com>
2025-03-04 11:16:56 +0700

    feat: add active header when scrolling

--- js/utils.js
+++ js/utils.js
@@ -13,4 +13,30 @@ document.addEventListener("DOMContentLoaded", function () {
             burgerMenu.classList.remove("active");
         });
     });
+
+    function updateActiveSection() {
+        let maxVisibleRatio = 0;
+        let activeSection = null;
+        const sections = Array.from(document.querySelectorAll("div[id]")).filter(div => ["about", "skills", "experiences", "awards", "interests"].includes(div.id))
+
+        sections.forEach(section => {
+            const rect = section.getBoundingClientRect();
+            const visibleHeight = Math.max(0, Math.min(rect.bottom, window.innerHeight) - Math.max(rect.top, 0));
+            const visibilityRatio = visibleHeight / section.offsetHeight;
+
+            if (visibilityRatio > maxVisibleRatio) {
+                maxVisibleRatio = visibilityRatio;
+                activeSection = section;
+            }
+        });
+
+        if (activeSection) {
+            document.querySelectorAll(".navbar a").forEach(
+                headerItem => headerItem.classList.remove("active")
+            )
+            document.querySelector(`.navbar a[href="#${activeSection.id}"]`).classList.add("active");
+        }
+    }
+
+    window.addEventListener("scroll", updateActiveSection);
 });
